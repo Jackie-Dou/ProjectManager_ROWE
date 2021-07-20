f39ce435f7bfa2c70071bc054962ceddb5e8e375
kasach katerina <kasach1477@mail.ru>
2021-07-20 18:28:25 +0300

    Add seede fot statuses

---
+++ database/seeders/TaskStatusSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TaskStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $statuses = ['новый', 'в работе', 'на тестировании', 'завершен'];
+
+        foreach ($statuses as $status) {
+            $newStatus = new TaskStatus();
+            $newStatus->fill(['name' => $status]);
+            $newStatus->save();
+        }
+    }
+}
